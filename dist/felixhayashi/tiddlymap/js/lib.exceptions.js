c422697f65bc187bc915f7828341438ddc2da35e
Felix Hayashi <ninshinobi@msn.com>
2016-08-08 22:34:47 +0200

    bumped dist files

--- dist/felixhayashi/tiddlymap/js/lib.exceptions.js
+++ dist/felixhayashi/tiddlymap/js/lib.exceptions.js
@@ -7,30 +7,4 @@ module-type: library
 @preserve
 
 \*/
-
-/*jslint node: true, browser: true */
-/*global $tw: false */
-"use strict";
-
-/*** Exports *******************************************************/
-
-module.exports = {};
-
-/*** Code **********************************************************/
-  
-var exception = module.exports;
-
-exception.EnvironmentError = function(aspect) {
-  this.name = "EnvironmentError";
-  this.message = "Critical parts of the underlying system changed: " + aspect;
-};
-
-exception.DependencyError = function(dep) {
-  this.name = "DependencyError";
-  this.message = "TiddlyMap cannot run without: " + dep;
-};
-  
-for(var ex in exception) {
-  exception[ex].prototype = Object.create(Error.prototype);
-  exception[ex].constructor = exception[ex];
-}
+"use strict";module.exports={};var exception=module.exports;exception.EnvironmentError=function(e){this.name="EnvironmentError";this.message="Critical parts of the underlying system changed: "+e};exception.DependencyError=function(e){this.name="DependencyError";this.message="TiddlyMap cannot run without: "+e};for(var ex in exception){exception[ex].prototype=Object.create(Error.prototype);exception[ex].constructor=exception[ex]}
